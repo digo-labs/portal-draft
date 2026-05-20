70918b47a132c5860ea0b1feb102a220f7181572
ljrrt <ljrrt.dev@gmail.com>
2026-05-20 13:59:12 -0300

    sync template

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,10 +7,15 @@ const rdsCa = readFileSync('./certs/rds-global-bundle.pem', 'utf8');
 
 const dbUrl = new URL(process.env.DATABASE_URL_MONOREPO!);
 
+const schemaName = readFileSync('./src/db.ts', 'utf8').match(/defineDatabases\(\s*['"]([^'"]+)['"]/)?.[1];
+
+if (!schemaName) throw new Error('drizzle.config: schema name not found in ./src/db.ts');
+
 export default {
   schema: './src/db.ts',
   out: './drizzle',
   dialect: 'postgresql',
+  schemaFilter: [schemaName],
   dbCredentials: {
     host: dbUrl.hostname,
     port: Number(dbUrl.port),
